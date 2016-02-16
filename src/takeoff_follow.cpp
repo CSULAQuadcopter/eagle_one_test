730f95afb31b33799fbfa31b295c916cf45ece6b
Jay Ess <mentoce@gmail.com>
2016-02-15 19:21:02 -0800

    created function to get tag distance in y-direction

--- src/takeoff_follow.cpp
+++ src/takeoff_follow.cpp
@@ -6,18 +6,11 @@ This program launches the AR Drone.
 It is intended as a simple example for those starting with the AR Drone platform.
 */
 #include <ros/ros.h>
-#include <cmath>
 #include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
-
-const double ALPHA = 0.0;
-const double GAMMA_2 = 1.2743; // radians
-const double ZETA = tan(GAMMA_2);
 //#include <ardrone_autonomy/NavdataMessageDefinitions.h>
 
 std_msgs::Empty emp_msg;
-
-double getTagDistanceY(double y, double alt);
 
 int main(int argc, char** argv)
 {
@@ -55,11 +48,3 @@ int main(int argc, char** argv)
 	}//ros::ok loop
 
 }//main
-
-
-
-double getTagDistanceY(double y, double alt)
-{
-	double beta = atan(ZETA * (1 - y/500));
-	return alt * tan(ALPHA + beta);
-}
