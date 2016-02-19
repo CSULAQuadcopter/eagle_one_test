bc9f6cfa66d51a756b3deeed09d4153545c29996
josh <mentoce@gmail.com>
2016-02-19 15:44:31 -0800

    removed all references to ROS publishing

--- include/eagle_one_test/Drone.h
+++ include/eagle_one_test/Drone.h
@@ -12,7 +12,6 @@ class Drone: public ARNavdata
 {
 public:
     Drone();
-    Drone(ros::NodeHandle nh);
     ~Drone();
 
     void setMode(int);
@@ -26,9 +25,6 @@ public:
 
     void set_navdata(const ardrone_autonomy::Navdata::ConstPtr&);
     void set_odometry(const nav_msgs::Odometry::ConstPtr&);
-    void follow_tag(const geometry_msgs::Twist::ConstPtr&);
-
-    void on_timer();
 
     void print_tag_distance();                 // Prints (x, y) of tag (mm, mm)
     void print_tag_x_distance();               // Prints x distance of tag (mm)
@@ -39,7 +35,7 @@ public:
     double calcDistanceZ(double z);            // tag z distance (mm)
     double calcYawDistance(double yaw);        // yaw (rad)
 
-    // Flight commands
+/*    // Flight commands
     void takeoff();
     void land();
     void emergency();
@@ -49,19 +45,13 @@ public:
     void moveZ(double);
     void changeYaw(double);
     void takePicture();
-    double follow(double);
-
+    double follow(double);*/
 
 private:
     int mode_;
     int tag_x_distance_;
     int tag_y_distance_;
     int tag_z_distance_;
-
-    geometry_msgs::Twist twist_msg;     // velocities
-    ros::NodeHandle node;
-    ros::Timer timer;
-    ros::Publisher publish;
 };
 
 #endif /* DRONE_H */
