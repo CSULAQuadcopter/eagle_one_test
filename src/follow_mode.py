8ace39f496a5a6a0ad8a7c163229f41ba977a7f2
jay3ss <mentoce@gmail.com>
2016-04-01 17:15:40 -0700

    started position control

--- src/follow_mode.py
+++ src/follow_mode.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python
 import rospy
 
+# We're using a thirdparty PID
+from pid_controller import PID
+
 from std_msgs.msg import String, Empty
 from geometry_msgs.msg import Twist
 from ardrone_autonomy.msg import Navdata
@@ -31,21 +34,59 @@ class YawControl(object):
         if(msg.tags_count > 0):
             self.yaw = msg.tags_orientation[0]
 
+class PositionControl(PID):
+    def __init__(self, P=0.5, I=0.0, D=0.1, Derivator=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
+
+        self.Kp=P
+        self.Ki=I
+        self.Kd=D
+        self.Derivator=Derivator
+        self.Integrator=Integrator
+        self.Integrator_max=Integrator_max
+        self.Integrator_min=Integrator_min
+
+        self.set_point=0.0
+        self.error=0.0
+
+
+tag_x = 0
+tag_y = 0
+
+def navdata_callback(msg):
+    if(tags_count > 0):
+        tag_x = msg.tags_xc[0]
+        tag_y = msg.tags_yc[0]
 
 def main():
-    yaw = Twist()
-    yaw.angular.x = 0.5
-    yaw.angular.y = 0.5
+    qc = Twist()
+    # Disable hover mode
+    qc.angular.x = 0.5
+    qc.angular.y = 0.5
 
     follow_yaw = YawControl(350, 10, 0.5)
 
+    pid_x = PositionControl()
+    pid_y = PositionControl()
+
+    # set the setpoints
+    pid_x.setPoint(500)
+    pid_y.setPoint(500)
+
+    x_update = 0
+    y_update = 0
+
     rospy.init_node('follow_mode')
     rate = rospy.Rate(100) # 100Hz
-    pub_yaw = rospy.Publisher('cmd_vel', Twist, queue_size=100)
+    pub_qc = rospy.Publisher('cmd_vel', Twist, queue_size=100)
+    sub_navdata = rospy.Subscriber('ardrone/navdata', Navdata, navdata_callback)
 
     while not rospy.is_shutdown():
-        yaw.angular.z = follow_yaw.check_yaw()
-        pub_yaw.publish(yaw)
+        x_update = pid_x.update(tag_x)
+        y_update = pid_y.update(tag_y)
+        qc.linear.x = pid_x.avoid_drastic_corrections(x_update)
+        qc.linear.y = pid_x.avoid_drastic_corrections(x_update)
+        qc.angular.z = follow_yaw.check_yaw()
+        pub_qc.publish(qc)
         rate.sleep()
 
 
