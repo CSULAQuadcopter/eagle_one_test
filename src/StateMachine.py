ae128a3f8557d278cd8b6a41f374d63fdd2c257a
David <drojas14@calstatela.edu>
2016-04-15 03:38:06 -0700

    fixed statemachine logic to enable all transition states

--- src/StateMachine.py
+++ src/StateMachine.py
@@ -6,13 +6,13 @@ from transitions import Machine
 # ROS imports
 import rospy
 from std_msgs.msg import String
-from eagle_one_test.srv import *
+from eagle_one_test.srv import State
 
 class Smach(Machine):
     def __init__(self):
         # Define the different states of the machine
         states = ['secure', 'takeoff', 'follow', 'take_picture', \
-                  'land', 'emergency', 'reacquisition']
+                  'land', 'reacquisition', 'emergency']
 
         # Define the transitions between states
         # FORMAT: ['trigger_event', 'source_state', 'destination_state']
@@ -22,12 +22,17 @@ class Smach(Machine):
             ['picture_command', 'follow', 'take_picture'],
             ['picture_taken', 'take_picture', 'land'],
             ['land_alt_reached', 'land', 'secure'],
-            ['emergency_condition', ['takeoff', 'follow', 'land'], 'emergency'],
-            ['tag_lost', ['takeoff', 'follow', 'land'], 'reacquisition'],
+            ['emergency_condition', ['takeoff', 'follow', 'take_picture', 'land'], 'emergency'],
+            ['takeoff_tag_lost', 'takeoff', 'reacquisition'],
+            ['follow_tag_lost', 'follow', 'reacquisition'],
+            ['land_tag_lost', 'land', 'reacquisition'],
+            ['take_picture_tag_lost', 'take_picture', 'reacquisition'],
             ['takeoff_tag_found', 'reacquisition', 'takeoff'],
             ['follow_tag_found', 'reacquisition', 'follow'],
             ['land_tag_found', 'reacquisition', 'land'],
-            ['timed_out', 'reacquisition', 'emergency']
+            ['take_picture_tag_found', 'reacquisition', 'take_picture'],
+            ['timed_out', 'reacquisition', 'emergency'],
+            ['reset', 'emergency', 'secure']
         ]
 
         Machine.__init__(self, states=states, transitions=transitions, \
@@ -45,23 +50,64 @@ class Smach(Machine):
     def change_state(self, transition):
         if((self.is_secure()) and (transition == 'TAKEOFF_COMMAND')):
             self.takeoff_command()
-        elif((self.is_takeoff()) and (transition == 'TAKEOFF_ALT_REACHED')):
-            self.takeoff_alt_reached()
-        elif((self.is_follow()) and (transition == 'PICTURE_COMMAND')):
-            self.picture_command()
-        elif((self.is_take_picture()) and (transition == 'PICTURE_TAKEN')):
-            self.picture_taken()
-        elif((self.is_land()) and (transition == 'LAND_ALT_REACHED')):
-            self.land_alt_reached()
-        elif(self.is_takeoff() or self.is_follow() or self.is_land()):
+        elif(self.is_takeoff()):
+            if(transition == 'TAKEOFF_ALT_REACHED'):
+                self.takeoff_alt_reached()
+                print("Altitude Goal Reached")
+            if(transition == 'TAKEOFF_TAG_LOST'):
+                self.takeoff_tag_lost()
+                print("Tag lost during takeoff")
             if(transition == 'EMERGENCY_CONDITION'):
                 self.emergency_condition()
-        elif(self.is_reacquisition() and (transition == 'TIMED_OUT')):
-            self.timed_out()
+                print("Emergency During Takeoff")
+        elif(self.is_follow()):
+            if(transition == 'PICTURE_COMMAND'):
+                self.picture_command()
+                print("Set to Take Picture")
+            if(transition == 'FOLLOW_TAG_LOST'):
+                self.follow_tag_lost()
+                print("Tag lost during follow")
+            if(transition == 'EMERGENCY_CONDITION'):
+                self.emergency_condition()
+                print("Emergency During Follow")
+        elif(self.is_take_picture()):
+            if(transition == 'PICTURE_TAKEN'):
+                self.picture_taken()
+                print("Picture Saved")
+            if(transition == "EMERGENCY_CONDITION"):
+                self.emergency_condition()
+                print("Emergency During Picture Attempt")
+        #this can be taken out, transition from taking picture to reacquisition if tag is lost
+        #during picture taking
+            if(transition == "TAKE_PICTURE_TAG_LOST"):
+                self.take_picture_tag_lost()
+                print("Tag Lost After Picture Taking")
+        elif(self.is_land()):
+            if(transition == 'LAND_ALT_REACHED'):
+                self.land_alt_reached()
+                print("Prepare to Land")
+            if(transition == 'LAND_TAG_LOST'):
+                print("Tag lost during landing")
+                self.land_tag_lost()
+            if(transition == 'EMERGENCY_CONDITION'):
+                self.emergency_condition()
+                print("Emergency During Landing")
+        elif((self.is_emergency()) and (transition == 'RESET')):
+            print("Return to secure mode")
+            self.reset()
         elif(self.is_reacquisition()):
             if(transition == 'TAKEOFF_TAG_FOUND'):
                 self.takeoff_tag_found()
+                print("Tag Found, Returning to Takeoff mode")
             if(transition == 'FOLLOW_TAG_FOUND'):
                 self.follow_tag_found()
+                print("Tag Found, Returning to Follow Mode")
             if(transition == 'LAND_TAG_FOUND'):
                 self.land_tag_found()
+                print("Tag Found, Returning to Land Mode")
+            if(transition == 'TAKE_PICTURE_TAG_FOUND'):
+                self.take_picture_tag_found()
+                print("Tag Found, Returning to Take Picture Mode")
+            if(transition == 'TIMED_OUT'):
+                self.timed_out()
+                print("Reacquisition Timer Expired, Entering Emergency Mode")
