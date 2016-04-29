611da21334b6d84b8f6c56029dcaee3e2c40ac3a
Jay Ess <jay3ss@users.noreply.github.com>
2016-04-28 18:36:37 -0700

    Update Emergency.py
    Inheriting from the Mode classnow

--- src/Emergency.py
+++ src/Emergency.py
@@ -7,27 +7,20 @@ from Mode import Mode
 # We're going to need to use String and Empty messages
 from std_msgs.msg import String, Empty
 
-# For integration with the qc_smach_server
-import qc_smach_client as sc
-
-class Emergency(object):
+class Emergency(Mode):
     def __init__(self):
         # Subscribe to the state machine transition topic
-        # TODO remove this
-        self.sub_transition = rospy.Subscriber('qc_smach/transitions', String, self.transCallback)
+        self.sub_transition = rospy.Subscriber('/qc_smach/transition', String, self.transition_cb)
 
         # Allow the mode to publish a land command
         self.pub_land = rospy.Publisher('/ardrone/land', Empty, queue_size=100)
-
-        # Initialize the transition
-        self.transition = ""
 
     def emergency_land(self):
         # Land!
         self.pub_land.publish(Empty())
         print("Emergency detected, Landing immediately")
 
-    def transCallback(self, msg):
+    def transition_cb(self, msg):
         self.transition = msg.data
 
     def goto_secure(self):
