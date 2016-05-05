d084fe84765e48b23e66d2552d8c3335db6d570c
Jay Ess <mentoce@gmail.com>
2016-05-05 12:51:51 -0700

    can now normalize tag position

--- src/Navdata.py
+++ src/Navdata.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 """
-Navdata
-Written by: Josh Saunders
-Date 4/11/2016
-This is to hold the navdata transmitted by the QC
+This is to hold the navdata transmitted by the QC and calculate the normalized
+position of the tags.
+
+Created by: Josh Saunders
+Date Created: 4/11/2016
+
+Modified by: Josh Saunders
+Date Modified: 5/5/2016
 """
 import rospy
 from ardrone_autonomy.msg import Navdata
 
 import math
+import normalize_position as norm
 
 class navdata_info(object):
     def __init__(self):
@@ -16,20 +21,36 @@ class navdata_info(object):
         self.sub_navdata = rospy.Subscriber('/ardrone/navdata', Navdata, self.callback)
         self.tag_acquired = False
         self.theta = 0
-        # These are actually switched for the controller purposes
+        # Euler angles
+        self.roll = 0
+        self.pitch = 0
+        # Altitude
+        self.altd = 0
+        # These are switched for the controller purposes
+        # Raw values taken from the QC
         self.tag_x = 0
         self.tag_y = 0
+        # These are switched for the controller purposes
+        # Calculated, normalized values
+        self.tag_norm_x = 0
+        self.tag_norm_y = 0
         # For the polar controller
         self.r     = 0
 
     def callback(self, data):
         self.navdata = data
+        self.altd = data.altd
+        self.roll = data.rotY
+        self.pitch = data.rotX
         if data.tags_count > 0:
             self.tag_acquired = True
             self.theta = self.navdata.tags_orientation[0]
             # These are actually switched for the controller purposes
             self.tag_x = self.navdata.tags_yc[0]
             self.tag_y = self.navdata.tags_xc[0]
+            # Calculate the normalized values
+            self.tag_norm_x = norm.real_position(self.tag_x, self.altd, self.pitch)
+            self.tag_norm_y = norm.real_position(self.tag_y, self.altd, self.roll)
             self.r     = math.hypot(self.tag_x, self.tag_y)
         else:
             self.tag_acquired = False
---
+++ src/normalize_position.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+"""
+Normalizes the tag. Without this, from the perspective of the QC, the tag will
+move when either pitching or rolling. By using some trig, the tag will appear
+to be stationary when pitching and rolling.
+
+Much of this information was sourced from:
+
+http://ludep.com/tracking-algorithm-condsidering-the-inclination-of-the-drone
+
+When using this, take care to set the origin of the image to be the center of
+the image (i.e. both x and y of the image should be [-500px, +500px])
+
+NOTE:
+    Inputs:
+    position: [-500px, +500px]
+    tilt: the tilt of the bottom camera
+    altitude: the altitude of the camera
+
+    Output:
+    position_actual: the calculated distance in mm
+
+    Formulas used:
+    cvt_alt         = altitude * tan(32 degrees/500px)
+    position_mm     = position * cvt_alt
+    position_actual = position_mm / cos(tilt) - altitude * tan(tilt)
+
+Created by: Josh Saunders and Amando Aranda
+Date Created: 5/5/2016
+
+Modified by:
+Date Modified:
+"""
+import math
+
+alpha   = math.radians(32)
+
+def real_position(position, altitude, tilt):
+    cvt_alt         = altitude * tan(32 degrees/500px)
+    position_mm     = position * cvt_alt
+    # position_actual
+    return position_mm / cos(tilt) - altitude * tan(tilt)
